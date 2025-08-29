f53a8632406600ebc1a96f7fb9708d556268ebb8
onronder <onuronder@hotmail.com>
2025-08-29 17:54:24 +0300

    fixes and deploy

--- duru_notes_app/ios/Runner/AppDelegate.swift
+++ duru_notes_app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
-import Flutter
 import UIKit
+import Flutter
+import UserNotifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,37 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // Register all Flutter plugins for the main isolate.
     GeneratedPluginRegistrant.register(with: self)
+
+    // Show notifications while app is in foreground (iOS 10+)
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+    }
+
+    // ---- Background isolate plugin registration (e.g. flutter_local_notifications) ----
+    // Some plugins spin up a background Flutter engine. For those engines to find
+    // your plugins, we pass a registrar block that calls GeneratedPluginRegistrant.
+    if let pluginClass = NSClassFromString("FlutterLocalNotificationsPlugin") as? NSObject.Type {
+
+      // Block that registers all plugins on a given registry.
+      let registrarBlock: (@convention(block) (FlutterPluginRegistry) -> Void) = { registry in
+        GeneratedPluginRegistrant.register(with: registry)
+      }
+
+      // Support both the new and legacy selector names.
+      let newSelector = NSSelectorFromString("setRegisterPlugins:")
+      let oldSelector = NSSelectorFromString("setPluginRegistrantCallback:")
+
+      if pluginClass.responds(to: newSelector) {
+        _ = pluginClass.perform(newSelector, with: registrarBlock)
+      } else if pluginClass.responds(to: oldSelector) {
+        _ = pluginClass.perform(oldSelector, with: registrarBlock)
+      }
+    }
+    // -------------------------------------------------------------------------------
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
