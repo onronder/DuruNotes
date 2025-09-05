3017d50dcb971ccd9323ded3591383f331d10355
onronder <onuronder@hotmail.com>
2025-09-05 15:39:17 +0300

    gemini fixes

--- duru_notes_app/ios/ci_scripts/ci_post_clone.sh
+++ duru_notes_app/ios/ci_scripts/ci_post_clone.sh
@@ -4,26 +4,28 @@ set -x
 
 echo "Starting ci_post_clone.sh script..."
 
-# Flutter'ın stabil versiyonunu kuruyoruz
+# Flutter'ın stabil (stable) versiyonunu kuruyoruz
 echo "Installing Flutter..."
 git clone https://github.com/flutter/flutter.git --depth 1 --branch stable /Users/local/flutter
 export PATH="/Users/local/flutter/bin:$PATH"
 
-# Flutter'ın doğru kurulduğunu kontrol ediyoruz
+# Flutter'ın doğru bir şekilde kurulduğunu ve ortam değişkenlerinin ayarlandığını kontrol ediyoruz
+echo "Verifying Flutter installation..."
 flutter doctor -v
 
-# Proje ana dizinine gidiyoruz
-cd ..
+# Projenin ana dizinine gidiyoruz
+cd $CI_WORKSPACE
 
-# Flutter projesini temizleyip bağımlılıkları yüklüyoruz
+# Flutter projesini temizleyip pub paketlerini yüklüyoruz
 echo "Cleaning Flutter project and getting dependencies..."
 flutter clean
 flutter pub get
 
-# iOS dizinine geri dönüyoruz
+# iOS dizinine gidip CocoaPods kurulumunu yapıyoruz
+echo "Navigating to iOS directory..."
 cd ios
 
-# CocoaPods entegrasyonunu sıfırlayıp yeniden kuruyoruz
+# CocoaPods entegrasyonunu sıfırlayıp yeniden kurarak olası cache sorunlarını önlüyoruz
 echo "De-integrating and re-installing CocoaPods..."
 pod deintegrate
 pod install --repo-update
