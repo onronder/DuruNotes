26a8f7688c97f429dea47fb8bc872b90edfe3918
onronder <onuronder@hotmail.com>
2025-09-05 16:17:33 +0300

    gemini fixes3

--- duru_notes_app/ios/ci_scripts/ci_post_clone.sh
+++ duru_notes_app/ios/ci_scripts/ci_post_clone.sh
@@ -1,39 +1,39 @@
 #!/bin/sh
 
-# Hata durumunda script'in durmasını ve komutları göstermesini sağlar
+# Hata durumunda script'in hemen durmasını ve çalıştırılan komutları terminalde göstermesini sağlar.
 set -e
 set -x
 
 echo "### Starting ci_post_clone.sh script..."
+# Script'in doğru yerden çalıştığını teyit etmek için mevcut konumu yazdırıyoruz.
+echo "### Script is running from: $(pwd)"
 
-# Flutter'ın stabil (stable) versiyonunu kuruyoruz
+# Flutter'ın stabil (stable) versiyonunu kuruyoruz.
 echo "### Installing Flutter SDK..."
 git clone https://github.com/flutter/flutter.git --depth 1 --branch stable /Users/local/flutter
 export PATH="/Users/local/flutter/bin:$PATH"
 
-# Flutter kurulumunu doğruluyoruz
+# Flutter kurulumunu ve versiyonunu doğruluyoruz.
 echo "### Verifying Flutter installation..."
 flutter doctor -v
 
-# Projenin ana çalışma dizinine gidiyoruz ($CI_WORKSPACE, Xcode Cloud'un proje dosyalarını klonladığı yerdir)
-echo "### Navigating to project root directory..."
-cd $CI_WORKSPACE
+# Flutter projesinin kök dizinine gitmemiz gerekiyor.
+# Script'imiz şu an 'duru_notes_app/ios/ci_scripts' içinde olduğundan, 3 dizin yukarı çıkmalıyız.
+cd ../../..
 
-# Flutter bağımlılıklarını temizleyip yeniden yüklüyoruz
+# Doğru dizinde olduğumuzu kontrol edelim.
+echo "### Now in Flutter project root: $(pwd)"
+
+# Flutter projesini temizleyip pub paketlerini yüklüyoruz.
 echo "### Cleaning and getting Flutter dependencies..."
 flutter clean
 flutter pub get
 
-# ÖNEMLİ ADIM: Flutter'a iOS projesini yeniden yapılandırmasını ve hazırlamasını söylüyoruz.
-# Bu komut, podspec bulunamadı hatasını kesin olarak çözer.
-echo "### Preparing iOS project for build..."
-flutter build ios --no-codesign
+# iOS klasörüne geri dönüyoruz.
+echo "### Navigating back to iOS directory..."
+cd duru_notes_app/ios
 
-# iOS klasörüne gidiyoruz
-echo "### Navigating to iOS directory..."
-cd ios
-
-# CocoaPods entegrasyonunu tamamen sıfırlayıp, güncel podları kuruyoruz
+# Son olarak CocoaPods kurulumunu yapıyoruz.
 echo "### Resetting and installing CocoaPods..."
 pod deintegrate
 pod install --repo-update
